13e116e98fca7e7c1e0a5f834de19972ee517039
onefen123 <3006139@qq.com>
2019-04-01 11:03:27 +0800

    111

--- src/main/java/com/zsgc/entity/Demo.java
+++ src/main/java/com/zsgc/entity/Demo.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.annotation.JSONField;
 
 import java.util.Date;
 
+@Data
 public class Demo {
     private int id;
     private String name;
@@ -12,45 +13,4 @@ public class Demo {
     @JSONField(format="yyyy-MM-dd")
     private Date timetime;
 
-
-    public Date getTimetime() {
-        return timetime;
-    }
-
-
-    public void setTimetime(Date timetime) {
-        this.timetime = timetime;
-    }
-
-    public String getMail() {
-        return mail;
-    }
-
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
